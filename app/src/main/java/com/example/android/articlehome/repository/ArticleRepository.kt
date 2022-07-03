01d6f6f1898b7d84a313c11c151c6351d8ffbbc8
Joseph Hamdi <joseph7amdy@gmail.com>
2022-07-03 15:22:46 +0200

    sa

---
+++ app/src/main/java/com/example/android/articlehome/repository/ArticleRepository.kt
@@ -0,0 +1,19 @@
+package com.example.android.articlehome.repository
+
+import androidx.lifecycle.LiveData
+import com.example.android.articlehome.database.ArticleDataBase
+import com.example.android.articlehome.models.ArticleModel
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+
+class ArticleRepository (private val asteroidDataBase:ArticleDataBase){
+
+    var allSavedArticles: LiveData<List<ArticleModel>> =asteroidDataBase.articleDao.getAllArticles()
+    suspend fun insertArticle(article:ArticleModel){
+        withContext(Dispatchers.IO){
+            asteroidDataBase.articleDao.insertAllArticle(article)
+        }
+    }
+}
